c15d193ef2d84157973303742a0df299413d43d0
dodoarg <50408939+dodoarg@users.noreply.github.com>
2019-11-19 19:11:17 +0100

    Add files via upload

---
+++ 1.2/SetD.m
@@ -0,0 +1,26 @@
+function [Dmean,Dvar] = SetD(bins)
+D = [];
+for rep = 1:10000
+   p1 = randi([1,20]);
+   p2 = p1;
+   while (p2 == p1)
+       p2 = randi([1,20]);
+   end
+   filename1 = sprintf('person%02d.mat',p1);
+   filename2 = sprintf('person%02d.mat',p2);
+   iris1 = load(filename1);
+   iris2 = load(filename2);
+   iris1 = iris1.iriscode;
+   iris2 = iris2.iriscode;
+  rdmf = randi([1,size(iris1,1)]);
+  hamming = HD(iris1(rdmf,:),iris2(rdmf,:));
+  D = [D hamming];
+end
+histogram(D,bins,'FaceColor','r')
+xlabel('HD')
+ylabel('count')
+legend;
+hold on
+Dmean = mean(D);
+Dvar = var(D);
+end
---
+++ 1.2/main.m
@@ -0,0 +1,4 @@
+function dum = main;
+[Smean,Svar] = SetS(7)
+[Dmean,Dvar] = SetD(7)
+end
