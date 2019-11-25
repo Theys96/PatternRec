097ee3d9e8c29cd376eafa4c9cf13003675d280f
dodoarg <50408939+dodoarg@users.noreply.github.com>
2019-11-25 14:47:48 +0100

    enhanced dc finder

--- 1.2/main.m
+++ 1.2/main.m
@@ -4,4 +4,6 @@ codes = load_iriscodes(20,'person%02d.mat');
 [Dmean,Dvar] = SetD(codes,10000,true)
 DoF = (Dmean*(1-Dmean))/Dvar %effective degrees of freedom
 title('HDs for iris codes from same (S) and different (D) individuals','FontSize',15);
+dc = find_dc(Dmean,Dvar,0.0005)
+frr = 1-normcdf(dc,Smean,sqrt(Svar)) %false rejection rate
 end
