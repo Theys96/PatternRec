1f1dbd749d6d2e96feb24eccf514bb790b767422
Thijs Havinga <thijs@thijshavinga.nl>
2019-12-08 15:59:27 +0100

    KNN edit

--- lab3/knn/KNN.m
+++ lab3/knn/KNN.m
@@ -5,6 +5,7 @@ function class = KNN(input, K, data, labels)
         error("K cannot be larger or equal to the number of data points, %d >= %d!", K, length(data));
     end
     
+    % Compute all distances
     dists = zeros(length(data),1);
     for i = 1:length(data)
         a = input-data(i,:);
@@ -12,7 +13,7 @@ function class = KNN(input, K, data, labels)
     end
     
     [~,idx] = sort(dists(:,1));  % Nearest candidates
-    labels = labels(idx);      % Sorted
-    class = mode(labels(1:K));  % Most frequent class
+    labels = labels(idx);        % Sorted
+    class = mode(labels(1:K));   % Most frequent class
 end
 
